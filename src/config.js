6cf567537d97f9b9a316771d072e431ab5d2a8b6
tomaszpankowski <pankowski.email@gmail.com>
2022-01-20 19:41:12 +0100

    modified routing

--- src/config.js
+++ src/config.js
@@ -1,15 +1,25 @@
 import Comingsoon from "./views/comingsoon";
 import Contact from "./views/contact";
+import Dashboard from "./views/dashboard";
 import Error from "./views/error";
+import History from "./views/history";
+import Home from "./views/home";
 import Login from "./views/login";
 import PrivacyPolicy from "./views/privacy";
 import Register from "./views/register";
+import Stats from "./views/stats";
+import Trade from "./views/trade";
 
 const appCfg = {
     routes:[
-        {navItem:false, exact:true, path:"/",name:"Comingsoon",view:<Comingsoon/>},
-        {navItem:false, exact:true, path:"/contact",name:"Contact",view:<Contact/>},
-        {navItem:false, exact:true, path:"/login",name:<span className="fa fa-user-circle"></span>,view:<Login backLink={{name:" Register now!",href:"/register"}}/>},
+        {navItem:true, exact:true, path:"/",name:"Home",view:<Home/>},
+        {navItem:true, exact:true, path:"/stats",name:"Stats",view:<Stats/>},
+        {navItem:true, exact:true, path:"/dashboard",name:"Dashboard",view:<Dashboard/>},
+        {navItem:true, exact:true, path:"/trade",name:"Trade",view:<Trade/>},
+        {navItem:true, exact:true, path:"/history",name:"History",view:<History/>},        
+        {navItem:true, exact:true, path:"/contact",name:"Contact",view:<Contact/>},
+        {navItem:true, exact:true, path:"/login",name:<span className="fa fa-user-circle"></span>,view:<Login backLink={{name:" Register now!",href:"/register"}}/>},        
+        {navItem:false, exact:true, path:"/comingsoon",name:"Comingsoon",view:<Comingsoon/>},
         {navItem:false, exact:true, path:"/register",name:"Register",view:<Register backLink={{name:" Login now!",href:"/login"}}/>},
         {navItem:false, exact:false, path:"/error",name:"Error",view:<Error backLink={{name:"back to Home",href:"/"}}/>},
         {navItem:false, exact:true, path:"/privacy",name:"Privacy",view: <PrivacyPolicy backLink={{name:"back to Home",href:"/"}}/>},
---
+++ src/views/trade.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+
+class Trade extends Component{
+    render(){
+        return(        
+            <Container fluid className="trade-s1 d-flex align-items-center minh-100vh">
+                
+            </Container>    
+        );
+    }
+}
+
+export default Trade;
