b931ac674efcdbefeb949ebd27891e2cfb94a416
Tm <pankowski.email@gmail.com>
2022-06-17 20:28:59 +0200

    frontend

--- src/views/trade.js
+++ src/views/trade.js
@@ -1,11 +1,14 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import TradeS1 from "../components/trade-s1";
+import TradeS2 from "../components/trade-s2";
 
 class Trade extends Component{
     render(){
         return(        
-            <Container fluid className="trade-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <TradeS1/>
+                <TradeS2/>
             </Container>    
         );
     }
